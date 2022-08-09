4e2c979f3dd729a170db746cb6210701685e79c9
JamosW <104772245+JamosW@users.noreply.github.com>
2022-08-08 23:26:13 -0400

    updated data

--- src/Data.js
+++ src/Data.js
@@ -6,7 +6,7 @@ export default [
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "Kaieteur Falls is the world's largest single drop waterfall by water volume. Located on the Potaro River in the Kaieteur National Park, it sits in a section of the Amazon rainforest included in the Potaro-Siparuni region of Guyana.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
     source: "GuyanaKaieteurFalls2004.jpg"
   },
@@ -14,10 +14,10 @@ export default [
     title: "Pyramid of Giza",
     location: "Egypt",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    startDate: "2 Jun, 2020",
+    endDate: "24 Aug, 2020",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "The Great Pyramid of Giza is the largest Egyptian pyramid and the tomb of Fourth Dynasty pharaoh Khufu. Built in the 26th century BC during a period of around 27 years.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
     source: "great-pyramid.jpg"
   },
@@ -25,8 +25,8 @@ export default [
     title: "Mount Fuji",
     location: "Japan",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    startDate: "5 Jan, 2020",
+    endDate: "24 Jan, 2020",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
